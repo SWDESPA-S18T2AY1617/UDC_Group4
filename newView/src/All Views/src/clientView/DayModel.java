e28e081c72ed81b0a6188835841ed3da8e777ca0
michaelryanrespicio <michael_respicio@dlsu.edu.ph>
2017-04-06 10:18:49 +0800

    Revert "pulling"
    This reverts commit 956f176488f47536142c98a8d8308b424c4d15e1.

---
+++ newView/src/All Views/src/clientView/DayModel.java
@@ -0,0 +1,19 @@
+package clientView;
+
+import javax.swing.table.*;
+
+public class DayModel extends DefaultTableModel{
+	 public boolean isCellEditable(int rowIndex, int mColIndex) {
+	        return true;
+	    }
+
+	    public DayModel() {
+	        String[] headers = {"Time", "Appointment"}; //All headers
+	        for (int i = 0; i < 2; i++) {
+	            this.addColumn(headers[i]);
+	        }
+	        
+	        this.setColumnCount(2);
+	        this.setRowCount(48);
+	    }
+}
---
+++ newView/src/All Views/src/clientView/Writer.java
@@ -0,0 +1,18 @@
+package clientView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Writer {
+    
+	public void write(String a) throws FileNotFoundException{
+
+		PrintWriter pw = new PrintWriter(new File("test2.csv"));
+        
+        pw.print("");
+		
+        pw.write(a);
+        pw.close();
+    }
+}
