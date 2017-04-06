6d73cad7fd2f8da1636e54886bb9400990e33c74
Jan Bertel Ngo <jan_bertel_ngo@dlsu.edu.ph>
2017-04-06 10:27:59 +0800

    one View

---
+++ src/view/CalendarView.java
@@ -0,0 +1,94 @@
+package view;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+
+public class CalendarView extends JPanel {
+	private final JLabel monthLabel;
+	private final JLabel yearLabel;
+	private final JButton btnPrev;
+	private final JButton btnNext;
+	private final JButton btnCreate;
+	private final JScrollPane scrollCalendarTable;
+	private final JComboBox<String> cmbYear;
+	private final JTable calendarTable;
+
+	public void initialize() {
+		setBorder(BorderFactory.createTitledBorder("Calendar"));
+
+		add(getMonthLabel());
+		add(getYearLabel());
+		add(getBtnPrev());
+		add(getBtnNext());
+		add(getBtnCreate());
+		add(getCmbYear());
+		add(scrollCalendarTable);
+
+		getCalendarTable().getParent().setBackground(getCalendarTable().getBackground()); // Set
+																							// background
+		getCalendarTable().getTableHeader().setResizingAllowed(false);
+		getCalendarTable().getTableHeader().setReorderingAllowed(false);
+
+		getCalendarTable().setColumnSelectionAllowed(true);
+		getCalendarTable().setRowSelectionAllowed(true);
+		getCalendarTable().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		getCalendarTable().setRowHeight(15);
+
+		getCmbYear().setBounds(120, 25, 90, 30);
+		getBtnPrev().setBounds(15, 183, 50, 30);
+		getBtnNext().setBounds(155, 183, 50, 30);
+		scrollCalendarTable.setBounds(20, 60, 195, 113);
+		getBtnCreate().setBounds(20, 100, 80, 35);
+	}
+
+	public CalendarView() {
+		monthLabel = new JLabel("try");
+		yearLabel = new JLabel();
+		btnPrev = new JButton("<<");
+		btnNext = new JButton(">>");
+		btnCreate = new JButton("SET");
+		calendarTable = new JTable();
+		cmbYear = new JComboBox<String>();
+		scrollCalendarTable = new JScrollPane(getCalendarTable());
+
+		this.initialize();
+
+		this.setSize(250, 220);
+		this.setLayout(null);
+	}
+
+	public JLabel getMonthLabel() {
+		return monthLabel;
+	}
+
+	public JLabel getYearLabel() {
+		return yearLabel;
+	}
+
+	public JButton getBtnPrev() {
+		return btnPrev;
+	}
+
+	public JButton getBtnNext() {
+		return btnNext;
+	}
+
+	public JButton getBtnCreate() {
+		return btnCreate;
+	}
+
+	public JComboBox<String> getCmbYear() {
+		return cmbYear;
+	}
+
+	public JTable getCalendarTable() {
+		return calendarTable;
+	}
+
+}
---
+++ src/view/WeekView.java
@@ -0,0 +1,59 @@
+package view;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.TableColumnModel;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.ListSelectionModel;
+
+
+public class WeekView extends JPanel{
+	private JTable weekTable;
+	private JScrollPane weekScroll;
+
+	/**
+	 * Create the panel.
+	 */
+	public WeekView(){
+		String[] columnNames = {"", "M", "T", "W", "H", "F"};
+		String[][] time = { { "0:00", "" }, { "", "" }, { "1:00", "" }, { "", "" }, { "2:00", "" }, { "", "" },
+				{ "3:00", "" }, { "", "" }, { "4:00", "" }, { "", "" }, { "5:00", "" }, { "", "" }, { "6:00", "" },
+				{ "", "" }, { "7:00", "" }, { "", "" }, { "8:00", "" }, { "", "" }, { "9:00", "" }, { "", "" },
+				{ "10:00", "" }, { "", "" }, { "11:00", "" }, { "", "" }, { "12:00", "" }, { "", "" }, { "13:00", "" },
+				{ "", "" }, { "14:00", "" }, { "", "" }, { "15:00", "" }, { "", "" }, { "16:00", "" }, { "", "" },
+				{ "17:00", "" }, { "", "" }, { "18:00", "" }, { "", "" }, { "19:00", "" }, { "", "" }, { "20:00", "" },
+				{ "", "" }, { "21:00", "" }, { "", "" }, { "22:00", "" }, { "", "" }, { "23:00", "" }, { "", "" } };
+
+		setWeekTable(new JTable(time, columnNames));
+		getWeekTable().setColumnSelectionAllowed(true);
+		getWeekTable().setCellSelectionEnabled(true);
+		getWeekTable().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		getWeekTable().setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		getWeekTable().setRowHeight(20);
+		getWeekTable().setBounds(0, 0, 400, 350);
+
+		TableColumnModel columnModel = getWeekTable().getColumnModel();
+		for(int i = 0; i<2; i++)
+			columnModel.getColumn(i).setPreferredWidth(50);
+		//columnModel.getColumn(1).setPreferredWidth(300);
+		setLayout(new BorderLayout());
+		
+
+		weekScroll = new JScrollPane(getWeekTable());
+		weekScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		weekScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		add(weekScroll);
+		
+	}
+
+	public JTable getWeekTable() {
+		return weekTable;
+	}
+
+	public void setWeekTable(JTable weekTable) {
+		this.weekTable = weekTable;
+	}
+}
